2111189e57191f38ed066a2bbfa13f0889b5b45a
vinayjagannavar <vjagannavar@gmail.com>
2024-04-06 16:35:13 -0400

    url added

--- src/main/java/com/zosh/config/AppConfig.java
+++ src/main/java/com/zosh/config/AppConfig.java
@@ -43,8 +43,7 @@ public class AppConfig {
 								"http://localhost:3000", 
 								"http://localhost:4000",
 								"http://localhost:4200",
-								"https://shopwithzosh.vercel.app",
-								"https://ecommerce-angular-blue.vercel.app/"
+								"https://clothcrafters.vercel.app"
 								
 							)
 						);
